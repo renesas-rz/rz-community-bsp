renesas-rz/rz-community-bsp

commit a56fef3e7f983ef24591437e298aa024d57303d5

meta-rz-bsp: u-boot: Add srecord-native as a dependency
Some Renesas platforms supported by mainline U-Boot use the srec_cat tool
to build recovery SPL binaries.

The srecord package provides srec_cat, so include it as a build dependency.

Signed-off-by: Chris Paterson <chris.paterson2@renesas.com>

meta-rz-bsp/recipes-bsp/u-boot/u-boot_2023.10.bb
@@ -2,6 +2,9 @@ require u-boot-renesas.inc
 
 COMPATIBLE_MACHINE = "rzg2h-family"
 
+# srec_cat is required for building ek874 as well as some other Renesas devices
+#DEPENDS += "srecord-native"
+
 SRCREV = "4459ed60cb1e0562bc5b40405e2b4b9bbf766d57"
 BRANCH = "master"
 UBOOT_URL = "git://source.denx.de/u-boot/u-boot.git"

meta-rz-bsp/recipes-bsp/u-boot/u-boot-renesas.inc
@@ -1,7 +1,7 @@
 require recipes-bsp/u-boot/u-boot-common.inc
 require recipes-bsp/u-boot/u-boot.inc
 
-DEPENDS += "python3-setuptools-native bc-native dtc-native lzop-native"
+DEPENDS += "python3-setuptools-native bc-native dtc-native lzop-native srecord-native"
 PROVIDES += "u-boot"
 
 SRC_URI = "${UBOOT_URL};protocol=https;branch=${BRANCH}"

commit 65d273e9fced462a6b7efdd21ac899e2adbf5ac5

meta-rz-bsp: u-boot: Remove commented out code
Fixes: a56fef3e7f98 ("meta-rz-bsp: u-boot: Add srecord-native as a dependency")

Signed-off-by: Chris Paterson <chris.paterson2@renesas.com>

meta-rz-bsp/recipes-bsp/u-boot/u-boot_2023.10.bb
@@ -2,9 +2,6 @@ require u-boot-renesas.inc
 
 COMPATIBLE_MACHINE = "rzg2h-family"
 
-# srec_cat is required for building ek874 as well as some other Renesas devices
-#DEPENDS += "srecord-native"
-
 SRCREV = "4459ed60cb1e0562bc5b40405e2b4b9bbf766d57"
 BRANCH = "master"
 UBOOT_URL = "git://source.denx.de/u-boot/u-boot.git"